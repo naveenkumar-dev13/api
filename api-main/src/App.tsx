1a6ca3a2f951eb7f0d4f92b318d628fde42859b2
naveenkumar-dev13 <naveenkumar.mfai@gmail.com>
2025-07-22 15:56:29 +0530

    zustand

--- api-main/src/App.tsx
+++ api-main/src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import ReactQuery from "./components/reactQuery";
+import Zustand from "./components/zustand";
 
 const App: React.FC = () => {
 
   return (
     <div>
-      <ReactQuery />
+      <Zustand/>
+      {/* <ReactQuery /> */}
     </div>
   );
 };
---
+++ api-main/src/components/zustand/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from './01-todo'
+
+function Zustand() {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  )
+}
+
+export default Zustand
